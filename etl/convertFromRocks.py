90edaa7a236684cf38ac46bd12ca9ae473dba651
Armin Sabouri <armin@team.casa>
2022-02-12 20:39:29 -0500

    Etl script to combine multiple rock dbs

--- etl/convertFromRocks.py
+++ etl/convertFromRocks.py
@@ -2,33 +2,38 @@
 
 import json
 import rocksdb
+import glob
 
-
-DB_PATH = '../data/raw_data.rocks'
+DB_PATH = '../data/'
+DB_GLOB = '/*.rocks'
 OUTPUT_PATH = './raw_data.json'
-
-opts = rocksdb.Options()
-opts.create_if_missing = True
-opts.max_open_files = 300000
-opts.write_buffer_size = 67108864
-opts.max_write_buffer_number = 3
-opts.target_file_size_base = 67108864
-
-opts.table_factory = rocksdb.BlockBasedTableFactory(
-    filter_policy=rocksdb.BloomFilterPolicy(10),
-    block_cache=rocksdb.LRUCache(2 * (1024 ** 3)),
-    block_cache_compressed=rocksdb.LRUCache(500 * (1024 ** 2)))
-
-db = rocksdb.DB(DB_PATH, opts)
 
 
 def convert_to_json_file():
-    it = db.iterkeys()
-    it.seek_to_first()
-    tx_ids = list(it)
+
     accumulated_list = []
-    for index, txid in enumerate(tx_ids):
-        accumulated_list.append(json.loads(db.get(txid)))
+    file_list = glob.glob(DB_PATH + DB_GLOB)
+    for rocks_db_path in file_list:
+        print(rocks_db_path)
+        opts = rocksdb.Options()
+
+        opts.create_if_missing = False
+        opts.max_open_files = 300000
+        opts.write_buffer_size = 67108864
+        opts.max_write_buffer_number = 3
+        opts.target_file_size_base = 67108864
+
+        opts.table_factory = rocksdb.BlockBasedTableFactory(
+            filter_policy=rocksdb.BloomFilterPolicy(10),
+            block_cache=rocksdb.LRUCache(2 * (1024 ** 3)),
+            block_cache_compressed=rocksdb.LRUCache(500 * (1024 ** 2)))
+        db = rocksdb.DB(rocks_db_path, opts)
+        it = db.iterkeys()
+        it.seek_to_first()
+        tx_ids = list(it)
+        for _index, txid in enumerate(tx_ids):
+            accumulated_list.append(json.loads(db.get(txid)))
+
     with open(OUTPUT_PATH, 'w') as f:
         json.dump(accumulated_list, f)
 
